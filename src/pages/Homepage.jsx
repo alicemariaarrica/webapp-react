463def92adc162fd2832115ec2adfcc5b87befae
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-16 21:23:18 +0100

    homepage-update-commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import MovieCard from '../components/MovieCard';
+
 
 function HomePage() {
     return (
         <div className="container mt-4">
             <h1>Lista dei Film</h1>
-
+            <div className="row">
+                {movies.map(movie => (
+                    <div className="col-md-4" key={movie.id}>
+                        <MovieCard movie={movie} />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
