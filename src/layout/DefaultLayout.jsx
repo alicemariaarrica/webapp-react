3efc4fed2d1af59f910b2a782d51e1f6e4e0baf4
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-17 23:29:26 +0100

    defaultlayout-commit

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,21 +1,20 @@
-import { Outlet } from "react-router-dom";
-import Header from '../components/Header';
-import Footer from '../components/Footer'
 
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function CreateDefaultLayout() {
-
     return (
         <>
             <header className="bg-primary text-white p-3">
-
+                <Header />
             </header>
-
-            <main>
-                <Outlet></Outlet>
+            <main className="container mt-4">
+                <Outlet />
             </main>
-
-            <footer></footer>
+            <footer className="bg-dark text-white p-3 mt-4">
+                <Footer />
+            </footer>
         </>
-    )
+    );
 }
