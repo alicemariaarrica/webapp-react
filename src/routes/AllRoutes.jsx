2d50e1c58cdf94afd6923fab2eb6b23c010f8717
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-17 23:26:06 +0100

    allroutes-commit

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -2,11 +2,13 @@
 import { Route, Routes } from 'react-router-dom';
 import CreateHomePage from '../pages/HomePage';
 import SpecificMoviePage from '../pages/SpecificMoviePage';
+import CreateDefaultLayout from '../layout/DefaultLayout';
 
 
 const CreateRoutes = () => (
     <Routes>
-        <Route path="/" element={<CreateHomePage />} />
+        <Route path="/" element={<CreateDefaultLayout />} />
+        <Route index element={<CreateHomePage />} />
         <Route path="/movie/:id" element={<SpecificMoviePage />} />
     </Routes>
 );
