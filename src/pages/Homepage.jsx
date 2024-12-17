8b5d25e7db646f5544ebebd7549d31f7b6131a66
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-17 23:31:30 +0100

    moviecard.jsx-commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import MovieCard from '../components/MovieCard';
 
+export default function CreateHomePage() {
+    const movies = [
+        { id: 1, title: 'Film 1', description: 'Descrizione del Film 1' },
+        { id: 2, title: 'Film 2', description: 'Descrizione del Film 2' },
+        { id: 3, title: 'Film 3', description: 'Descrizione del Film 3' }
+    ];
 
-export default CreateHomePage
+    return (
+        <div className="row">
+            {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+        </div>
+    );
+}
