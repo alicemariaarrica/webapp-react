cfe37217f5931c67d219144f33338c74e6dab2c1
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-17 20:21:40 +0100

    header.jx-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <header className="d-flex align-items-center justify-content-between p-2 bg-primary mb-4">
+                <h1>Logo </h1>
+                <h2>Free Movies</h2>
+            </header>
+        </>
+    )
+}
